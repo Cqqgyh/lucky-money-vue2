22c205a1f0ec95048f9e6f8a7633d6bc8817408c
cqq <945327638@qq.com>
2023-06-30 13:54:41 +0800

    feat: 页面路由路径

---
+++ src/views/luckyMoney/luckyMoney.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home-container">
+    红包雨写法
+  </div>
+</template>
+<script>
+export default {
+  name: 'luckyMoneyPage',
+}
+
+</script>
+
+<style scoped lang="scss">
+.home-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 26vh;
+}
+</style>
