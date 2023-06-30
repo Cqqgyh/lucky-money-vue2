1c5e4942852b045c6ad6100e14e1f475b99142e4
cqq <945327638@qq.com>
2023-06-30 11:14:00 +0800

    feat: 引入axios并封装

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/dev-api': {// 匹配所有以 '/dev-api'开头的请求路径
+        target: 'http://139.198.163.91:8888',// 代理目标的基础路径
+        changeOrigin: true,
+        pathRewrite: {'^/dev-api': ''}
+      },
+    }
+  }
 })
