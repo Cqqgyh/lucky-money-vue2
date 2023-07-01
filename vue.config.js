260803234e86df79413d95d556cdea1623f931f3
cqq <945327638@qq.com>
2023-07-01 09:09:22 +0800

    feat: 红包类-为了避免生成红包重合，设计生成轨道，一般轨道设计为单个红包的宽度

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/dev-api': {// 匹配所有以 '/dev-api'开头的请求路径
-        target: 'http://139.198.163.91:8888',// 代理目标的基础路径
+        target: 'http://rb.atguigu.cn/',// 代理目标的基础路径
         changeOrigin: true,
         pathRewrite: {'^/dev-api': ''}
       },
