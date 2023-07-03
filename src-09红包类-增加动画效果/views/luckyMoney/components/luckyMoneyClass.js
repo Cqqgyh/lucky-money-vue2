6507d5afa3ebdecbaf10f70f47532b55c9a1fdbf
cqq <945327638@qq.com>
2023-07-03 15:22:18 +0800

    perf: 增加动画注释

--- src-09红包类-增加动画效果/views/luckyMoney/components/luckyMoneyClass.js
+++ src-09红包类-增加动画效果/views/luckyMoney/components/luckyMoneyClass.js
@@ -1,5 +1,21 @@
 // 导入默认图片地址
 import defaultUrl from '@/assets/images/luckyMoney.png'
+/**
+ * @description:动画属性
+ * animation: name duration timing-function delay iteration-count direction;
+ * animation-name 	规定需要绑定到选择器的 keyframe 名称。
+ * animation-duration 	规定完成动画所花费的时间，以秒或毫秒计。
+ * animation-timing-function 	规定动画的速度曲线。
+ * animation-delay 	规定在动画开始之前的延迟。
+ * animation-iteration-count 	规定动画应该播放的次数。
+ * animation-direction 	规定是否应该轮流反向播放动画。
+ * animation-fill-mode 	规定动画在执行时间之外应用的值。
+ *    none	不改变默认行为。
+ *    forwards	当动画完成后，保持最后一个属性值（在最后一个关键帧中定义）。
+ *    backwards	在 animation-delay 所指定的一段时间内，在动画显示之前，应用开始属性值（在第一个关键帧中定义）。
+ *    both	向前和向后填充模式都被应用。
+ * animation-play-state 	规定动画是正在运行还是暂停。
+ * */
 
 // 定义一个红包类
 export default class LuckyMoney {
